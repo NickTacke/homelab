6140ae94be2cdff5581ca9798835527241176672
NickTacke <nicktacke@icloud.com>
2019-10-18 12:07:10 +0200

    Update

---
+++ js/server/server.js
@@ -0,0 +1,38 @@
+var io1 = require('socket.io').listen(8001);
+const fetch = require('node-fetch');
+const fs = require('fs');
+
+io1.sockets.on('connection', function (socket) {
+
+    console.log("NEW USER!")
+
+    socket.on("getdata", (data) => {
+        console.log(data);
+        if (data == true) {
+            fetch('https://vexar.xyz/api/products.php')
+                .then(response => response.text())
+                .then((data) => {
+                    var split = data.split(" ");
+                    for(i = 0; i < split.length; i++){
+                        if(split[i].includes("%20")){
+                            console.log("true");
+                            var elsplit = split[i].split("%20");
+                            split[i] = elsplit.join(" ");
+                            console.log(split[i]);
+                        }
+                    }
+                    socket.emit("callbackGetData", split);
+                })
+        }
+    })
+
+    socket.on("addProduct", (data) =>{
+        console.log(data);
+        fetch('https://vexar.xyz/api/products.php?validation=' + data.validation + '&add=' + data.add)
+        .then((response) => response.text())
+        .then((data) =>{
+            console.log(data);
+            socket.emit("callbackAddProduct", data);
+        })
+    })
+});
