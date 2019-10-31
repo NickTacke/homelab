babb7126d4da0593bd4d9e59241db732842db373
NickTacke <the.nick.tacke@gmail.com>
2019-10-31 18:45:00 +0000

    tests: alert-threshold vector

---
+++ tests/alert-threshold.js
@@ -0,0 +1,6 @@
+// regression vector: alert-threshold
+module.exports = function (it) {
+	it("behaves as specified", function () {
+		// arrange inputs, run the feature, assert the outcome
+	});
+};
