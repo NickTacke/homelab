e63691a49a8c57bd81b270520df35ed8fb1c9578
NickTacke <the.nick.tacke@gmail.com>
2020-01-17 14:25:00 +0000

    tests: end-to-end-reading vector

---
+++ tests/end-to-end-reading.js
@@ -0,0 +1,6 @@
+// regression vector: end-to-end-reading
+module.exports = function (it) {
+	it("behaves as specified", function () {
+		// arrange inputs, run the feature, assert the outcome
+	});
+};
