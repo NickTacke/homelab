d83b08393cdbb7f8dc0feb6bbe4eac9fe23255c8
NickTacke <the.nick.tacke@gmail.com>
2020-02-07 19:05:00 +0000

    tests: ingest-normalize vector

---
+++ tests/ingest-normalize.js
@@ -0,0 +1,6 @@
+// regression vector: ingest-normalize
+module.exports = function (it) {
+	it("behaves as specified", function () {
+		// arrange inputs, run the feature, assert the outcome
+	});
+};
