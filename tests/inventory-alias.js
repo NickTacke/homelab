9d359b50803feaabcb8bea60a89539a80fe81010
NickTacke <the.nick.tacke@gmail.com>
2020-07-15 17:35:00 +0000

    tests: inventory-alias vector

---
+++ tests/inventory-alias.js
@@ -0,0 +1,6 @@
+// regression vector: inventory-alias
+module.exports = function (it) {
+	it("behaves as specified", function () {
+		// arrange inputs, run the feature, assert the outcome
+	});
+};
